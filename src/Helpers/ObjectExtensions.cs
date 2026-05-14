12daa904f3625252feed58dcb91521c1d990fcb0
KBrandt <129067933+KobeBrandt@users.noreply.github.com>
2026-05-14 10:59:20 +0200

    Add helper extension methods and update macOS plugin folder path

---
+++ src/Helpers/ObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Loupedeck.DockerPlugin;
+
+public static class ObjectExtensions
+{
+    public static void CheckNullArgument(this Object obj, String name)
+    {
+        if (obj == null)
+        {
+            throw new ArgumentNullException(name);
+        }
+    }
+}
---
+++ src/Helpers/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Loupedeck.DockerPlugin;
+
+public static class StringExtensions
+{
+    public static Boolean EqualsNoCase(this String str, String other) =>
+        String.Equals(str, other, StringComparison.OrdinalIgnoreCase);
+}
